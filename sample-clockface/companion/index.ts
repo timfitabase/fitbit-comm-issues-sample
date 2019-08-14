a88bbfe98e4fe779eb2aa6d76d0c459586796939
Tim Sutor <tim@fitabase.com>
2019-08-14 11:00:46 -0600

    added wake-interval calls

--- sample-clockface/companion/index.ts
+++ sample-clockface/companion/index.ts
@@ -1,7 +1,16 @@
 import ft from "./fileTransfer";
 import messaging from "./peerMessaging";
+import { me } from "companion"
 
 let init = function() {
+    const MILLISECONDS_PER_MINUTE = 1000 * 60;
+    me.wakeInterval = 5.5 * MILLISECONDS_PER_MINUTE;
+
+    if (me.launchReasons.wokenUp) {
+        // The companion started due to a periodic timer
+        console.log("Started due to wake interval!")
+      }
+
     let msgInterval = 180000;
     setInterval(function() {
         messaging.sendMessage("COMPANION MESSAGE " + Date.now(), {timeout: msgInterval});}, msgInterval);
