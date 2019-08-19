47ce2771121b0e35b852a1005cd8d678d7439735
Tim Sutor <tim@fitabase.com>
2019-08-19 15:06:42 -0600

    Added wake-interval and removed companion interval messaging

--- sample-clockface/companion/index.ts
+++ sample-clockface/companion/index.ts
@@ -2,18 +2,22 @@ import ft from "./fileTransfer";
 import messaging from "./peerMessaging";
 import { me } from "companion"
 
-let init = function() {
-    const MILLISECONDS_PER_MINUTE = 1000 * 60;
-    me.wakeInterval = 5.5 * MILLISECONDS_PER_MINUTE;
+const MILLISECONDS_PER_MINUTE = 1000 * 60;
+    me.wakeInterval = 50 * MILLISECONDS_PER_MINUTE;
 
     if (me.launchReasons.wokenUp) {
         // The companion started due to a periodic timer
-        console.log("Started due to wake interval!")
-      }
+        console.log("Started due to wake interval")
+        } else if (me.launchReasons.fileTransfer) {
+            console.log("Started due to file transfer")
+        }
 
-    let msgInterval = 180000;
-    setInterval(function() {
-        messaging.sendMessage("COMPANION MESSAGE " + Date.now(), {timeout: msgInterval});}, msgInterval);
+let init = function() {
+    
+
+    // let msgInterval = 180000;
+    // setInterval(function() {
+    //     messaging.sendMessage("COMPANION MESSAGE " + Date.now(), {timeout: msgInterval});}, msgInterval);
 
     ft.init();
 }
