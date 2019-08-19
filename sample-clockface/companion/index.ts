8685296df90ac1afcea47589d00bf76af43fd300
Tim Sutor <tim@fitabase.com>
2019-08-19 15:38:30 -0600

    Added event handler

--- sample-clockface/companion/index.ts
+++ sample-clockface/companion/index.ts
@@ -3,13 +3,17 @@ import messaging from "./peerMessaging";
 import { me } from "companion"
 
 const MILLISECONDS_PER_MINUTE = 1000 * 60;
-    me.wakeInterval = 5 * MILLISECONDS_PER_MINUTE;
+    me.wakeInterval = 5.1 * MILLISECONDS_PER_MINUTE;
 
     if (me.launchReasons.wokenUp) {
         // The companion started due to a periodic timer
         console.log("Started due to wake interval");
-        } else if (me.launchReasons.fileTransfer) {
-            console.log("Started due to file transfer");
+    } else if (me.launchReasons.fileTransfer) {
+        console.log("Started due to file transfer");
+    }
+
+    me.onwakeinterval = evt => {
+        console.log("Companion was already awake - onwakeinterval");
         }
 
 let init = function() {
