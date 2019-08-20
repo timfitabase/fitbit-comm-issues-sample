2e638ac2dffba56f04cdd6c7f26ccb0c80c3f9ba
Tim Sutor <tim@fitabase.com>
2019-08-20 09:26:20 -0600

    Additional debugging - send message on wake

--- sample-clockface/companion/index.ts
+++ sample-clockface/companion/index.ts
@@ -3,17 +3,19 @@ import messaging from "./peerMessaging";
 import { me } from "companion"
 
 const MILLISECONDS_PER_MINUTE = 1000 * 60;
-    me.wakeInterval = 5.1 * MILLISECONDS_PER_MINUTE;
+    me.wakeInterval = 6 * MILLISECONDS_PER_MINUTE;
 
     if (me.launchReasons.wokenUp) {
         // The companion started due to a periodic timer
         console.log("Started due to wake interval");
+        messaging.sendMessage("COMPANION STARTED DUE TO WAKE INTERVAL. " + Date.now());
     } else if (me.launchReasons.fileTransfer) {
         console.log("Started due to file transfer");
     }
 
     me.onwakeinterval = evt => {
         console.log("Companion was already awake - onwakeinterval");
+        messaging.sendMessage("onwakeinterval fired but companion was already running. " + Date.now());
         }
 
 let init = function() {
