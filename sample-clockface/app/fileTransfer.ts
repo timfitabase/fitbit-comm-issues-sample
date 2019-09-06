cd82cc5d4465a73228be7779ff257bd98791f807
Tim Sutor <tim@fitabase.com>
2019-09-06 11:11:33 -0600

    Refactored file transfer to get around error

--- sample-clockface/app/fileTransfer.ts
+++ sample-clockface/app/fileTransfer.ts
@@ -1,10 +1,9 @@
 import { outbox, inbox } from "file-transfer";
 import * as fs from "fs";
 
+const MAX_TRANSFER_COUNT:number = 5;
 
 let self : any = {};
-
-const MAX_TRANSFER_COUNT:number = 5;
 
 self.canTransfer = function() {
     try {
@@ -15,38 +14,83 @@ self.canTransfer = function() {
             return Promise.resolve(underLimit);
         });
     } catch (ex) {
-        console.log("Error in app canTransfer()");
+        console.log(ex);
         return Promise.reject(false);
     }
 }
 
+// self.deleteOldestQueuedTransfer = function(substringOfQueuedFileToReplace: string) {
+//     try {
+//     let promise = outbox.enumerate();
+//     return promise.then(fileTransferArray => {
+//         let matchingFile = fileTransferArray.sort().find(function(ft) {
+//             let idx = ft.name.indexOf(substringOfQueuedFileToReplace);
+//             return idx != -1;
+//         });
 
-self.transferFile = function(fileName : string){
-    try {
-        let fileSize = fs.statSync(fileName).size;
-        if(fileSize === 0) {
-            return Promise.reject(new Error("0 byte file, cannot transfer"));
-        }
+//         if (matchingFile) {
+//             console.log(`Cancelling ${matchingFile.name}`);
+//             matchingFile.cancel();
+//             return Promise.resolve(true);
+//         } else {
+//             return Promise.resolve(false);
+//         }
+//     });
+//     } catch (ex) {
+//         console.log(ex);
+//         return Promise.reject(ex);
+//     }
+// }
 
-        let promise = self.canTransfer();
-        return promise.then(function (underLimit:Boolean) {
-            if (underLimit) {
-                return queueFileTransfer(fileName);
-            } else {
-                console.log("Cannot queue file transfer, max transfer count reached");
-                return Promise.reject(new Error("Cannot queue file transfer, max transfer count reached"));
-            }
-        });
-    } catch(ex) {
-        console.log(ex);
-        return Promise.reject(new Error(ex));
-    }
-}
+// self.transferFile = function(fileName : string, substringOfQueuedFileToReplace: string = ""){
+//     try {
+//         let fileSize = fs.statSync(fileName).size;
+//         if(fileSize === 0) {
+//             return Promise.reject(new Error("0 byte file, cannot transfer"));
+//         }
 
-function queueFileTransfer(fileName:string) {
+//         return queueFileTransfer(fileName);
+
+//         let promise = self.canTransfer();
+//         return promise.then(function (underLimit:Boolean) {
+//             console.log("underLimit: " + underLimit);
+            
+//             if (underLimit) {
+//                 return queueFileTransfer(fileName);
+//             } else {
+//                 //For our most important transfers (prompt events), try to remove an old log file so we can stay under the queued file limit.
+//                 if (substringOfQueuedFileToReplace != "") {
+//                     //If we were able to delete, we'll fall through and initiate the transfer.
+//                     let deletePromise = self.deleteOldestQueuedTransfer(substringOfQueuedFileToReplace);
+//                     return deletePromise.then(function (success:Boolean) {
+//                         if (success) {
+//                             return queueFileTransfer(fileName);
+//                         } else {
+//                             let error = new Error("Cannot queue file transfer and no queued logs to purge");
+//                             console.log(error);
+//                             return Promise.reject(error);
+//                         }
+//                     })
+//                 }
+//                 else {
+//                     let error = new Error("Cannot queue file transfer, max transfer count reached");
+//                     console.log(error);
+//                     return Promise.reject(error);
+//                 }
+//             }
+//         });
+        
+//     } catch(ex) {
+//         console.log(ex);
+//         return Promise.reject(new Error(ex));
+//     }
+// }
+
+self.queueFileTransfer = function(fileName:string) {
     console.log(`trying to enqueue ${fileName}`);
+
     let promise = outbox.enqueueFile("/private/data/" + fileName);
-    return promise.then(() => {
+    return promise.then((ft) => {
         console.log("FileTransferToCompanionQueued");
         return Promise.resolve();
     })
--- sample-clockface/app/logFileTransfer.ts
+++ sample-clockface/app/logFileTransfer.ts
@@ -5,28 +5,37 @@ const LOG_FILE_NAME = "./resources/logFile.txt";
 
 export function exportLogs() {
     try {
-            console.log("Exporting test log file");
+        let promise = fileTransfer.canTransfer();
+        return promise.then(function (underLimit:Boolean) {
 
-            //Set a unique transfer file name so we don't overwrite any previously queued transfer files
-            let logTransferFileName = `${Date.now()}_logFile.txt`;
+            if (underLimit) {
+                console.log("Exporting test log file");
 
-            try {
-                let ab = fs.readFileSync(LOG_FILE_NAME);
-                fs.writeFileSync(logTransferFileName, ab);
-            } catch(e) {
-                console.log("Error renaming log file");
-                console.log(e);
-                return;
-            }
+                //Set a unique transfer file name so we don't overwrite any previously queued transfer files
+                let logTransferFileName = `${Date.now()}_logFile.txt`;
 
-            fileTransfer.transferFile(logTransferFileName)
-            .then(function() {
-                console.log("Export complete");
+                try {
+                    let ab = fs.readFileSync(LOG_FILE_NAME);
+                    fs.writeFileSync(logTransferFileName, ab);
+                } catch(e) {
+                    console.log("Error renaming log file");
+                    console.log(e);
+                    return;
+                }
+
+                fileTransfer.queueFileTransfer(logTransferFileName)
+                .then(function() {
+                    console.log("Export complete");
+                    fs.unlinkSync(logTransferFileName);
+                });
+                
                 fs.unlinkSync(logTransferFileName);
-            });
-            
-            fs.unlinkSync(logTransferFileName);
-        
+            } else {
+                let error = new Error("Cannot queue file transfer, max transfer count reached");
+                console.log(error);
+                return Promise.reject(error);
+            }
+        });
     } catch(ex) {
         console.log(ex);
     }
