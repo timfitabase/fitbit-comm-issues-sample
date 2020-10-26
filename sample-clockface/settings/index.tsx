46a62fb6173096216bb296afe5c4e756852f3442
Tim <tim@fitabase.com>
2020-10-26 15:28:07 -0600

    Added Settings props to change

--- sample-clockface/settings/index.tsx
+++ sample-clockface/settings/index.tsx
@@ -1,9 +1,20 @@
 function settingsComponent(props: any) {
+  let screenWidth = props.settingsStorage.getItem("screenWidth");
+  let screenHeight = props.settingsStorage.getItem("screenHeight");
   return (
     <Page>
       <Section title={<Text bold align="left">Setup</Text>}>
-        
+        <TextInput label="Test Input" settingsKey="testInput" />      
       </Section>
+      <ImagePicker
+          title="Background Image"
+          description="Pick an image to use as your background."
+          label="Pick a Background Image"
+          sublabel="Background image picker"
+          settingsKey="backgroundImage"
+          imageWidth={ screenWidth }
+          imageHeight={ screenHeight }
+      />
     </Page>
   );
 }
